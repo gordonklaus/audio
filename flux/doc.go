6fb6f861e5ee3fae5e1d9524889f41c1d88d1490
Gordon Klaus <gordon.klaus@gmail.com>
2014-05-14 20:03:06 +0200

    Flux:  Refined and expanded docs.

--- flux/doc.go
+++ flux/doc.go
@@ -21,27 +21,31 @@ Function editor
 
 The function editor displays a function or method as a kind of graph.  The nodes of the graph specify operations such as function calls and control flow.  Nodes typically have some inputs and outputs (generally, ports) by which they can be connected.  A connection has an output as its source and an input as its destination, indicating that a value is passed from the output to the input.  An input may have zero or more connections; the value used is the last one to have been passed or the zero value if none.
 
-Every node belongs to a block.  Outermost is the function block, all of whose nodes are executed when the function is called.  An if-node has an if-block and an else-block, each containing nodes that are conditionally executed.  A loop node has a loop block whose nodes are executed zero or more times.  A function literal node has a function block whose nodes are executed when the function value is called.
+Every node belongs to a block.  Outermost is the function block, which is run when the function is called.  An if-node has one or more blocks, one of which is conditionally run.  A loop node has a loop block that is run zero or more times.  A function literal node has a function block that is run when the function value is called.  A select node has zero or more cases consisting of a channel operation and a block; one of these channel operations is run followed by its block.
 
 The execution order of nodes is determined as follows:  Node A runs before node B if there is a connection with A as its source and B as its destination.  A connection that exits or enters a block has that block's containing node as a source or destination, respectively.
 
 The arrow keys are used to navigate the graph.  On their own, they move the focus along and between connections following the topology of the graph.  While holding Alt, they move the focus between nodes with no regard for connectivity.  Pressing Escape moves the focus from a connection end to its port, from a port to its node, or from a node to its containing node.  Pressing Escape when a top-level node is focused saves changes and exits the function editor.
 
-To create a named node (function or method, struct field, operator, special node), simply start typing its name; the browser will open, allowing you to select the desired item.  Hold Shift in the browser to treat functions and methods as values; otherwise they are treated as calls.
+To create a named node (function or method, variable, constant, struct field, operator, special node), simply start typing its name; the browser will open, allowing you to select the desired item.  Hold Shift in the browser to treat functions and methods as values; otherwise they are treated as calls.
 
-A struct field node can be toggled between read and write using the Equals key.
+A variable node or struct field node can be toggled between read and write using the Equals key.
 
 A method value node with an unconnected receiver is treated as a method expression.
 
-A variadic function or method call node can have inputs added by pressing the Comma key, and can be toggled between multiple element input and single slice input modes by pressing Control-Period.
+A variadic function or method call node can have inputs added by pressing the Comma key and deleted by pressing Delete or Backspace, and can be toggled between multiple element input and single slice input modes by pressing Control-Period.
 
-To create a numeric, string, or character literal node, type a digit, double quote, or single quote character, respectively, followed by the value and Enter.  To create a composite literal node, type a left curly brace character and select the desired type from the browser.
+To create a basic literal (numeric, string, or character) node, type a digit, double quote, or single quote character, respectively, followed by the value and Enter.  Press Enter to edit a basic literal node.  To create a composite literal node, type a left curly brace character and select the desired type from the browser.
 
-A function block always has at least two nodes, one for parameters and another for results.  To add a parameter or result, focus the appropriate node or port and press Comma (hold Shift to insert before a port), type the name and Enter, then select the type from the browser.  To delete a parameter or result, focus the port and press Backspace or Delete.  To toggle the signature's variadicity, focus the final parameter's port and press Control-Period.
+A function block always has at least two nodes, one for parameters and another for results.  To add a parameter or result, focus the appropriate node or port and press Comma (hold Shift to insert before a port), type the name and Enter, then select the type from the browser.  To delete a parameter or result, focus the port and press Backspace or Delete.  To toggle the signature's variadicity, focus the final parameter's port and press Control-Period.  To toggle between a pointer receiver and a value receiver on a method, focus the receiver's port and press '*'.
 
-To create a new connection, move the focus to a port and press Enter to start editing.  Use the arrow keys to move the other end of the connection and press Enter to stop editing.  To edit an existing connection, move the focus to one of its ends and press Enter.
+To add a block to an if-node or a case to a select node, press Comma; press Backspace or Delete to remove it.  To toggle a select case between send and receive, press Equals.  To turn a select case into the default case (provided one doesn't already exist), focus its channel port and press Backspace or Delete.
 
-As an alternative to being drawn as a line, a connection may be named by pressing Underscore and typing a name followed by Enter.
+To create a new connection, focus a port and press Enter to start editing.  Use the arrow keys to move the other end of the connection and press Enter to stop editing.  To edit an existing connection, focus one of its ends and press Enter.
+
+As an alternative to being drawn as a line, a connection may be named by pressing Underscore and typing a name followed by Enter.  Press Underscore to draw it as a line again.  All named connections having the same source share a name.
+
+To control the execution order of two nodes that are ambiguously ordered, a sequencing connection can be made.  Focus a node's sequencing input or output by pressing Alt-Shift-Up or Alt-Shift-Down, respectively; then, create a connection as usual.  A sequencing connection is drawn as a dashed line.
 
 Press Backspace or Delete to delete a node or connection.
 
@@ -54,6 +58,6 @@ The type editor displays a type as a tree.  Composite types have their children 
 
 Press Enter to move the focus from a composite type to one of its children.  Use the arrow keys to move the focus between the children of a composite type.  Press Escape to move the focus from a child to its parent.
 
-To edit the focused item, press Backspace.  For a named item (struct field, function parameter or result, and interface method), first type the name and Enter.  Otherwise just select the type from the browser.  After a composite type is created, each of its children will be edited.  Press Escape to stop entering new named items.  Press Comma to insert a new named item (hold Shift to insert before the focused item); to delete one, press Delete.
+To replace the focused item, press Backspace.  For a named item (struct field, function parameter or result, or interface method), first type the name and Enter.  Otherwise just select the type from the browser.  After a composite type is created, each of its children is edited in turn.  Press Escape to stop entering new named items.  Press Comma to insert a new named item (hold Shift to insert before the focused item); to delete one, press Delete.
 */
 package main
