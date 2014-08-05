b02a0a6f4c80753c4e2dd632a4ddc0cdeb2c77d2
Gordon Klaus <gordon.klaus@gmail.com>
2014-08-05 19:13:28 +0200

    audio:  Rename SineOsc to FixedFreqSineOsc, bring back SineOsc as a frequency-modulable oscillator, use them in the songs.

--- osc.go
+++ osc.go
@@ -7,30 +7,44 @@ import (
 
 type SineOsc struct {
 	Params Params
+	phase  float64
+	Out    Audio
+}
+
+func (o *SineOsc) Sine(freq Audio) Audio {
+	for i, f := range freq {
+		o.Out[i] = math.Sin(2 * math.Pi * o.phase)
+		_, o.phase = math.Modf(o.phase + f/o.Params.SampleRate)
+	}
+	return o.Out
+}
+
+type FixedFreqSineOsc struct {
+	Params Params
 	freq   float64
 	x, d   complex128
 	Out    Audio
 }
 
-func (o *SineOsc) InitAudio(p Params) {
+func (o *FixedFreqSineOsc) InitAudio(p Params) {
 	o.Params = p
 	o.SetFreq(o.freq)
 	o.Out.InitAudio(p)
 }
 
-func (o *SineOsc) SetFreq(freq float64) {
+func (o *FixedFreqSineOsc) SetFreq(freq float64) {
 	o.freq = freq
 	if o.x == 0 {
-		o.x = 1
+		o.SetPhase(0)
 	}
 	o.d = cmplx.Exp(complex(0, 2*math.Pi*freq/o.Params.SampleRate))
 }
 
-func (o *SineOsc) SetPhase(phase float64) {
+func (o *FixedFreqSineOsc) SetPhase(phase float64) {
 	o.x = cmplx.Exp(complex(0, 2*math.Pi*phase))
 }
 
-func (o *SineOsc) Sine() Audio {
+func (o *FixedFreqSineOsc) Sine() Audio {
 	for i := range o.Out {
 		o.Out[i] = real(o.x)
 		o.x *= o.d
