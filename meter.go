4fc319ca5d804b9acc877b5418e5f4d2102e3892
Gordon Klaus <gordon.klaus@gmail.com>
2014-09-03 15:00:46 +0200

    song1:  Added controls to reverb.  Factored out audiogui.Main.  Added audiogui.Write.

--- meter.go
+++ meter.go
@@ -2,28 +2,28 @@ package audio
 
 import "math"
 
-type AmpMeter struct {
+type RMS struct {
 	windowSize float64
 	buf        []float64
 	i          int
 	sum        float64
 }
 
-func NewAmpMeter(windowSize float64) *AmpMeter {
-	return &AmpMeter{windowSize: windowSize}
+func NewRMS(windowSize float64) *RMS {
+	return &RMS{windowSize: windowSize}
 }
 
-func (a *AmpMeter) InitAudio(p Params) {
+func (a *RMS) InitAudio(p Params) {
 	a.buf = make([]float64, int(p.SampleRate*a.windowSize))
 }
 
-func (a *AmpMeter) Add(x float64) {
+func (a *RMS) Add(x float64) {
 	a.sum -= a.buf[a.i]
 	a.buf[a.i] = x * x
 	a.sum += x * x
 	a.i = (a.i + 1) % len(a.buf)
 }
 
-func (a *AmpMeter) Amplitude() float64 {
+func (a *RMS) Amplitude() float64 {
 	return math.Sqrt(a.sum / float64(len(a.buf)))
 }
