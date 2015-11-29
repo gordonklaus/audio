fedd36b6bdff57534d11d099ed081c8a470f4d0c
Gordon Klaus <gordon.klaus@gmail.com>
2015-11-29 10:26:08 +0100

    audio:  Add an envelope and a couple of filters.

--- env.go
+++ env.go
@@ -45,3 +45,47 @@ func (e *AttackReleaseEnv) Sing() float64 {
 func (e *AttackReleaseEnv) Done() bool {
 	return e.release && e.x < .0001
 }
+
+type LinAttackReleaseEnv struct {
+	Params                  Params
+	attackTime, releaseTime float64
+	up, down                float64
+	release                 bool
+	x                       float64
+}
+
+func NewLinAttackReleaseEnv(attackTime, releaseTime float64) *LinAttackReleaseEnv {
+	return &LinAttackReleaseEnv{attackTime: attackTime, releaseTime: releaseTime}
+}
+
+func (e *LinAttackReleaseEnv) InitAudio(p Params) {
+	e.Params = p
+	e.SetAttackTime(e.attackTime)
+	e.SetReleaseTime(e.releaseTime)
+}
+
+func (e *LinAttackReleaseEnv) SetAttackTime(t float64) {
+	e.attackTime = t
+	e.up = 1 / (e.Params.SampleRate * t)
+}
+
+func (e *LinAttackReleaseEnv) SetReleaseTime(t float64) {
+	e.releaseTime = t
+	e.down = 1 / (e.Params.SampleRate * t)
+}
+
+func (e *LinAttackReleaseEnv) Attack()  { e.release = false }
+func (e *LinAttackReleaseEnv) Release() { e.release = true }
+
+func (e *LinAttackReleaseEnv) Sing() float64 {
+	if e.release {
+		e.x = math.Max(0, e.x-e.down)
+	} else {
+		e.x = math.Min(1, e.x+e.up)
+	}
+	return e.x
+}
+
+func (e *LinAttackReleaseEnv) Done() bool {
+	return e.release && e.x < .0001
+}
--- filter.go
+++ filter.go
@@ -16,3 +16,93 @@ func (f *DCFilter) Filter(x float64) float64 {
 	f.x = x
 	return f.y
 }
+
+type LinSmoother struct {
+	params                    Params
+	attackSpeed, releaseSpeed float64
+	up, down                  float64
+	x, y                      float64
+}
+
+func NewLinSmoother(attackSpeed, releaseSpeed, startValue float64) *LinSmoother {
+	return &LinSmoother{attackSpeed: attackSpeed, releaseSpeed: releaseSpeed, y: startValue}
+}
+
+func (e *LinSmoother) InitAudio(p Params) {
+	e.params = p
+	e.SetAttackSpeed(e.attackSpeed)
+	e.SetReleaseSpeed(e.releaseSpeed)
+}
+
+func (e *LinSmoother) Value() float64 {
+	return e.y
+}
+
+func (e *LinSmoother) SetValue(v float64) {
+	e.y = v
+}
+
+func (e *LinSmoother) SetAttackSpeed(s float64) {
+	e.attackSpeed = s
+	e.up = s / e.params.SampleRate
+}
+
+func (e *LinSmoother) SetReleaseSpeed(s float64) {
+	e.releaseSpeed = s
+	e.down = -s / e.params.SampleRate
+}
+
+func (e *LinSmoother) Smooth(x float64) float64 {
+	e.x = x
+	d := e.up
+	if e.x < e.y {
+		d = e.down
+	}
+	e.y += d
+	return e.y
+}
+
+func (e *LinSmoother) Done() bool {
+	return math.Abs(e.x - e.y) < .0001
+}
+
+type ExpSmoother struct {
+	params                  Params
+	attackTime, releaseTime float64
+	up, down                float64
+	x, y                    float64
+}
+
+func NewExpSmoother(attackTime, releaseTime float64) *ExpSmoother {
+	return &ExpSmoother{attackTime: attackTime, releaseTime: releaseTime}
+}
+
+func (e *ExpSmoother) InitAudio(p Params) {
+	e.params = p
+	e.SetAttackTime(e.attackTime)
+	e.SetReleaseTime(e.releaseTime)
+}
+
+func (e *ExpSmoother) SetAttackTime(t float64) {
+	e.attackTime = t
+	e.up = 1 - math.Pow(.01, 1/(e.params.SampleRate*t))
+}
+
+func (e *ExpSmoother) SetReleaseTime(t float64) {
+	e.releaseTime = t
+	e.down = 1 - math.Pow(.01, 1/(e.params.SampleRate*t))
+}
+
+func (e *ExpSmoother) Smooth(x float64) float64 {
+	e.x = x
+	a := e.up
+	if e.x < e.y {
+		a = e.down
+	}
+	e.y += a * (e.x - e.y)
+	return e.y
+}
+
+func (e *ExpSmoother) Done() bool {
+	return math.Abs(e.x - e.y) < .0001
+}
