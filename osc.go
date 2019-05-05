30cea0afda6b2b91c7370a1cbf03fc012facf7c0
Gordon Klaus <gordon.klaus@gmail.com>
2019-05-05 15:46:43 +0200

    Add phase modulated sine oscillators.

--- osc.go
+++ osc.go
@@ -46,6 +46,101 @@ func (o *SineOsc) Sing() float64 {
 	return imag(o.x)
 }
 
+type SinePM struct {
+	pidt  float64
+	freq  float64
+	step  float64
+	phase float64
+	pm    float64
+}
+
+func (o *SinePM) InitAudio(p Params) {
+	o.pidt = math.Pi / p.SampleRate
+	o.Freq(o.freq)
+}
+
+func (o *SinePM) Freq(freq float64) *SinePM {
+	o.freq = freq
+	o.step = 2 * freq * o.pidt
+	return o
+}
+
+func (o *SinePM) PM(pm float64) *SinePM {
+	o.pm = pm * math.Pi
+	return o
+}
+
+func (o *SinePM) Phase(phase float64) *SinePM {
+	o.phase = phase
+	return o
+}
+
+func (o *SinePM) Sing() float64 {
+	o.phase += o.step
+	if o.phase > math.Pi {
+		o.phase -= 2 * math.Pi
+	}
+	return math.Sin(o.phase + o.pm)
+}
+
+type SineSelfPM struct {
+	pidt  float64
+	freq  float64
+	index float64
+	x, d  complex128
+}
+
+func (o *SineSelfPM) InitAudio(p Params) {
+	o.pidt = math.Pi / p.SampleRate
+	if o.x == 0 {
+		o.x = 1
+	}
+	o.Freq(o.freq)
+}
+
+func (o *SineSelfPM) Freq(freq float64) *SineSelfPM {
+	o.freq = freq
+	return o
+}
+
+func (o *SineSelfPM) Index(i float64) *SineSelfPM {
+	o.index = i
+	return o
+}
+
+func (o *SineSelfPM) Phase(phase float64) *SineSelfPM {
+	o.x = cmplx.Exp(complex(0, 2*math.Pi*phase))
+	return o
+}
+
+func (o *SineSelfPM) Sing() float64 {
+	const (
+		maxStepSize = 0.05
+		maxSteps    = 100
+	)
+
+	wdt_2 := o.freq * o.pidt / (1 - o.index*real(o.x))
+	if wdt_2 < maxStepSize {
+		o.x *= o.expwdt_approx(wdt_2)
+		return imag(o.x)
+	}
+
+	steps := math.Min(maxSteps, math.Ceil(wdt_2/maxStepSize))
+	for i := 0.; i < steps; i++ {
+		wdt_2 := o.freq * o.pidt / ((1 - o.index*real(o.x)) * steps)
+		o.x *= o.expwdt_approx(wdt_2)
+	}
+
+	return imag(o.x)
+}
+
+func (o *SineSelfPM) expwdt_approx(wdt_2 float64) complex128 {
+	// an optimization of complex(1, wdt_2) / complex(1, -wdt_2):
+	wdt_22 := wdt_2 * wdt_2
+	_1wdt_22 := 1 / (1 + wdt_22)
+	return complex((1-wdt_22)*_1wdt_22, 2*wdt_2*_1wdt_22)
+}
+
 type SawOsc struct {
 	dt   float64
 	freq float64
@@ -60,6 +155,11 @@ func (o *SawOsc) InitAudio(p Params) {
 func (o *SawOsc) Freq(freq float64) *SawOsc {
 	o.freq = freq
 	o.d = 2 * freq * o.dt
+	return o
+}
+
+func (o *SawOsc) Phase(x float64) *SawOsc {
+	o.x = 2*x - 1
 	return o
 }
 
