2829b33c4964409746e151ae530561e3a95c4bc2
Gordon Klaus <gordon.klaus@gmail.com>
2020-10-17 22:38:46 +0200

    Update Play to work with WebAssembly instead of GopherJS.

--- play_js.go
+++ play_js.go
@@ -5,38 +5,61 @@ package audio
 import (
 	"errors"
 
-	"github.com/gopherjs/gopherjs/js"
+	"syscall/js"
 )
 
-var node *js.Object
+var node js.Value
+var callback js.Func
 
-func startPlaying(v Voice, c PlayControl) error {
-	contextType := js.Global.Get("AudioContext")
-	if contextType == js.Undefined {
-		contextType = js.Global.Get("webkitAudioContext")
+func startPlaying(v interface{}, c PlayControl) error {
+	contextType := js.Global().Get("AudioContext")
+	if contextType.IsUndefined() {
+		contextType = js.Global().Get("webkitAudioContext")
 	}
-	if contextType == js.Undefined {
+	if contextType.IsUndefined() {
 		s := "The Web Audio API is apparently not supported in this browser."
-		js.Global.Get("document").Call("write", "<p>"+s+"</p>")
+		js.Global().Get("document").Call("write", "<p>"+s+"</p>")
 		return errors.New(s)
 	}
 	context := contextType.New()
 	Init(v, Params{SampleRate: context.Get("sampleRate").Float()})
-	node = context.Call("createScriptProcessor", 1024, 0, 1)
-	node.Set("onaudioprocess", func(e *js.Object) {
-		out := e.Get("outputBuffer").Call("getChannelData", 0).Interface().([]float32)
-		for i := range out {
-			out[i] = float32(v.Sing())
-		}
-		if v.Done() {
-			c.Stop()
-		}
-	})
+	switch v := v.(type) {
+	case Voice:
+		node = context.Call("createScriptProcessor", 16384, 0, 1)
+		callback = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+			out := args[0].Get("outputBuffer").Call("getChannelData", 0)
+			for i := 0; i < out.Length(); i++ {
+				out.SetIndex(i, v.Sing())
+			}
+			if v.Done() {
+				c.Stop()
+			}
+			return nil
+		})
+	case StereoVoice:
+		node = context.Call("createScriptProcessor", 16384, 0, 2)
+		callback = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+			out := args[0].Get("outputBuffer")
+			left := out.Call("getChannelData", 0)
+			right := out.Call("getChannelData", 1)
+			for i := 0; i < left.Length(); i++ {
+				l, r := v.Sing()
+				left.SetIndex(i, l)
+				right.SetIndex(i, r)
+			}
+			if v.Done() {
+				c.Stop()
+			}
+			return nil
+		})
+	}
+	node.Set("onaudioprocess", callback)
 	node.Call("connect", context.Get("destination"))
 	return nil
 }
 
 func stopPlaying() error {
 	node.Call("disconnect")
+	callback.Release()
 	return nil
 }
